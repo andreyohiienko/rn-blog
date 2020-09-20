8b8eb9d2ac815c5861ef86b5c82c92fa5e8dbea9
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-09-20 14:21:19 +0300

    Fix initial setup

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
-const { IndexScreen } = require('./src/screens/IndexScreen')
+import IndexScreen from './src/screens/IndexScreen'
 import { BlogProvider } from './src/context/BlogContext'
 
 const navigator = createStackNavigator(
